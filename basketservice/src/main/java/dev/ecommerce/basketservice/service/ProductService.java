18ee648de85b8ef577b6b55a36d283305e8056ed
igorpandolf <pandolf.igor@gmail.com>
2026-07-20 16:01:22 -0300

    cria Caching de dados externos com Redis

--- basketservice/src/main/java/dev/ecommerce/basketservice/service/ProductService.java
+++ basketservice/src/main/java/dev/ecommerce/basketservice/service/ProductService.java
@@ -3,6 +3,7 @@ package dev.ecommerce.basketservice.service;
 import dev.ecommerce.basketservice.client.PlatziStoreClient;
 import dev.ecommerce.basketservice.client.response.PlatziProductResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,11 +14,13 @@ public class ProductService {
 
     private final PlatziStoreClient platziStoreClient;
 
+    @Cacheable(value = "products")
     public List<PlatziProductResponse> getAllProducts() {
         return platziStoreClient.getAllProducts();
     }
 
-    public PlatziProductResponse getProductById(Long id) {
-        return platziStoreClient.getProductById(id);
+    @Cacheable(value = "product", key = "#productId")
+    public PlatziProductResponse getProductById(Long productId) {
+        return platziStoreClient.getProductById(productId);
     }
 }
