b1b1e3b6850b43236ce33cbc910f06e9864c958f
igorpandolf <pandolf.igor@gmail.com>
2026-07-20 15:08:29 -0300

    first commit

---
+++ basketservice/src/main/java/dev/ecommerce/basketservice/service/ProductService.java
@@ -0,0 +1,23 @@
+package dev.ecommerce.basketservice.service;
+
+import dev.ecommerce.basketservice.client.PlatziStoreClient;
+import dev.ecommerce.basketservice.client.response.PlatziProductResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final PlatziStoreClient platziStoreClient;
+
+    public List<PlatziStoreClient> getAllProducts() {
+        return platziStoreClient.getAllProducts();
+    }
+
+    public PlatziProductResponse getProductById(Long id) {
+        return platziStoreClient.getProductById(id);
+    }
+}
